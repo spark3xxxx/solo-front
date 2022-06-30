5abf36b0efa20b70d50ab47d3ae93111dbcef509
sadakiawada <spark3xxx@gmail.com>
2022-06-30 17:52:10 +0900

    feat: add noodle pasta jsx

---
+++ src/components/Pasta.jsx
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
+
+function Pasta() {
+  const [pastas, setPasta] = useState([]);
+
+  useEffect(() => {
+    pasta();
+  }, []);
+
+  const pasta = async () => {
+    const db = await fetch("http://127.0.0.1:8000/foods", {
+      credentials: "include", // ここを追加。
+    });
+
+    const data = await db.json();
+    setPasta(data);
+  };
+  return (
+    <div>
+      <Wrapper>
+        <h3>Pasta</h3>
+        <Splide
+          options={{
+            perPage: 3,
+            arrows: true,
+            pagination: false,
+            drag: "free",
+            gap: "5rem",
+          }}
+        >
+          {pastas.map((recipe) => {
+            return (
+              <SplideSlide key={recipe._id}>
+                <Card>
+                  <p>{recipe.name}</p>
+
+                  <img src={recipe.img} alt={recipe.img} />
+                  <Gradient />
+                </Card>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wrapper>
+    </div>
+  );
+}
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+  position: relative;
+
+  img {
+    border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    text-align: center;
+    font-weight: 600;
+    fot-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-item: center;
+  }
+`;
+
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+`;
+
+export default Pasta;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Noodle from "../components/Noodle";
+import Pasta from "../components/Pasta";
+
+function Home() {
+  return (
+    <div>
+      <Noodle />
+      <Pasta />
+    </div>
+  );
+}
+
+export default Home;
